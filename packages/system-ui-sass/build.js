eccf87f5f7be056733e568e95de7e2399486108f
Antoine Girard <thetoine@gmail.com>
2020-01-31 16:01:02 -0500

    Utility class ready for : Display, Flex, Sizings, Radius, Visibility, Overflow, Colors

---
+++ packages/system-ui-sass/build.js
@@ -0,0 +1,57 @@
+// TODO: clean up code here
+
+// build.js
+const sass = require("node-sass");
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require(`path`);
+
+const jsonToSassVars = require('./utils/jsonToSassVars');
+
+const SRC_PATH = path.join(__dirname, `./src`);
+const DIST_PATH = path.join(__dirname, `./dist`);
+const INPUT_FILENAME = `system-ui.scss`;
+const OUTPUT_FILENAME = `system-ui.css`;
+
+const PATHS = {
+  "main": path.resolve(SRC_PATH, INPUT_FILENAME),
+  "theme": path.resolve(`theme.js`),
+  "dist": path.resolve(DIST_PATH, OUTPUT_FILENAME)
+};
+
+// load theme file
+const theme = require(PATHS['theme']);
+
+
+// Read main SCSS content
+fs.readFile(PATHS[`main`], (err, data) => {
+
+  if(err) {
+    console.error('err:', err)
+    return;
+  }
+
+  // get file source as string
+  const source = data.toString();
+
+  // console.log(jsonToSassVars(theme));
+
+  sass.render({
+    // concat parsed JSON theme variables with SCSS raw string source
+    data: jsonToSassVars(theme) + source,
+    outFile: PATHS[`dist`],
+    // outputStyle: `compressed`,
+    includePaths: [SRC_PATH],
+  },function(err, result) {
+    if (!err) {
+     fs.writeFile(PATHS[`dist`], result.css, function(fsErr) {
+      if (!fsErr) console.log(`${chalk.green('SUCCESS!')} ${OUTPUT_FILENAME} file generated!`);
+      else console.log(`${chalk.red('ERROR!')} During ${OUTPUT_FILENAME} file generation: ${err}`);
+     });
+    } else {
+     console.log(`${chalk.red('ERROR!')} During ${OUTPUT_FILENAME} file generation: ${err}`);
+    }
+  });
+
+})
+
---
+++ packages/system-ui-sass/utils/jsonToSassVars.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function(obj) {
+
+  function jsonToSassVars(obj) {
+    // Make object root properties into sass variables
+    let sassString = '';
+
+    Object.keys(obj).forEach(key => {
+      const val = JSON.stringify(obj[key], null);
+      sassString += `$${key}:${val};\n`;
+    });
+
+    if (!sassString) {
+      return sassString;
+    }
+
+    // Store string values (so they remain unaffected)
+    const storedStrings = [];
+    sassString = sassString.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, function (str) {
+
+      const id = '___JTS' + storedStrings.length;
+      storedStrings.push({ id: id, value: str });
+      return id;
+    });
+
+    // Convert js lists and objects into sass lists and maps
+    sassString = sassString.replace(/[{\[]/g, '(').replace(/[}\]]/g, ')');
+
+    // Put string values back (now that we're done converting)
+    storedStrings.forEach(function (str) {
+      str.value = str.value.replace(/["']/g, '');
+      sassString = sassString.replace(str.id, str.value);
+    });
+
+    return sassString;
+  }
+
+  return jsonToSassVars(obj)
+
+}
