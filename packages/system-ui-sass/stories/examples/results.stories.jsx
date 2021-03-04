4b4c6c5e84442b2f88796ab9204eca7b1e0d0f14
Antoine Girard <thetoine@gmail.com>
2021-03-03 20:45:01 -0500

    tweaks in grid story

--- packages/system-ui-sass/stories/examples/results.stories.jsx
+++ packages/system-ui-sass/stories/examples/results.stories.jsx
@@ -2,24 +2,20 @@ import style from "../../dist/system-ui.css";
 import theme from "../../theme.babel.js";
 import { Logo } from './index'
 
-export default { title: "Examples/Items" };
+export default { title: "Examples/Results" };
 
 import { VerticalResultItem } from './items.stories';
 
-export const Results = () => `
+export const Grid = () => `
   <style>
   .MyResults {
-
   }
   </style>
   <section class="px-2 px-lg-6">
-
-
     <header class="ta-center py-8">
-      <h1 class="fw-300 m-0">My great section of products!</h1>
+      <h4 class="gray-500 mt-0 mb-3">Tag line!</h4>
+      <h1 class="heading-h2 color-primary fw-300 m-0">My great section of products!</h1>
     </header>
-
-
     <div class="d-grid grid-column-md-2 grid-column-lg-3 grid-gap-4">
       ${VerticalResultItem()}
       ${VerticalResultItem()}
@@ -28,6 +24,5 @@ export const Results = () => `
       ${VerticalResultItem()}
       ${VerticalResultItem()}
     </div>
-
   </section>
 `
