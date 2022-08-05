4921a22127c54027e9b595dc8166cfb601abfddf
Antoine Girard <thetoine@gmail.com>
2022-08-05 09:02:40 -0400

    downgraded Storybook packages

--- packages/system-ui-sass/.storybook/main.js
+++ packages/system-ui-sass/.storybook/main.js
@@ -2,13 +2,13 @@ module.exports = {
   stories: ['../stories/**/*.stories.@(jsx|mdx)'],
   addons: [
     {
-      name: '@storybook/addon-storysource'
+      name: '@storybook/addon-viewport'
     },
     {
-      name: '@storybook/addon-viewport'
+      name: '@storybook/addon-storysource'
     },
     {
       name: '@whitespace/storybook-addon-html'
     }
-  ]
+  ],
 };
