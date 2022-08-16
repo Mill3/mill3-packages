b8b0d5b7bb6b9d81243f5b30dc26b56604f3e36a
ConstanceSou <hello@constancesouville.com>
2022-08-16 17:06:58 -0400

    erratum last commit

--- packages/system-ui-sass/stories/examples/grids.stories.jsx
+++ packages/system-ui-sass/stories/examples/grids.stories.jsx
@@ -37,7 +37,7 @@ export const CrossGridLayout = () => `
   <section class="p-md-30 p-lg-50 bg-gray-500">
     <div class="d-sm-grid grid-column-2 grid-column-lg-3 grid-gap-20 overflow-hidden">
 
-      <header class="col-start-1 col-end-2 col-end-span-lg-3 d-block overflow-hidden position-relative vh-100 vh-md-50">
+      <header class="col-start-1 col-end-limit d-block overflow-hidden position-relative vh-100 vh-md-50">
         <hgroup class="position-absolute b-0 l-0 z-1000 w-100 w-xl-50 p-20 p-lg-50 color-white">
           <h1 class="fz-3xl fz-md-5xl fz-xl-6xl fw-300 ff-heading m-0">My kinda complex grid layout</h1>
           <h4 class="mt-10 mb-0 fw-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quia modi vel delectus? Quidem, quam. Esse facilis alias doloremque consequatur veritatis enim ullam deserunt, sed, corrupti repellat eum aliquam provident?</h4>
@@ -46,7 +46,7 @@ export const CrossGridLayout = () => `
         <img class="image-as-background" src="https://images.unsplash.com/photo-1614769507210-bf9482e2f32f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80" alt="unsplash"/>
       </header>
 
-      <div class="col-start-1 col-end-2 col-end-span-lg-1 pl-20 pl-md-0 pr-20 pr-md-0 pt-30 pb-30 d-flex flex-column justify-content-between gray-700">
+      <div class="col-start-1 col-end-limit col-end-span-lg-1 pl-20 pl-md-0 pr-20 pr-md-0 pt-30 pb-30 d-flex flex-column justify-content-between gray-700">
         <h3>Section title</h3>
         <footer class="w-lg-50">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quaerat, fugiat esse, aut repudiandae minima pariatur dolores eveniet unde nulla doloribus incidunt non obcaecati modi cupiditate labore, aliquid maiores atque.</p>
